38dc06f10d5998cd2ef462a99f6077858ebf4123
Jose Carlos <jc443559@gmail.com>
2026-03-03 20:53:54 -0300

    Css da tela de inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import { RoomContext } from "../contexts/RoomContext"
 import { useSocket } from "../hooks/useSocket"
 import { useNavigate } from "react-router-dom"
+import "../styles/Home.css"
 
 function Home() {
   const { setRoomCode } = useContext(RoomContext)
@@ -20,9 +21,13 @@ function Home() {
   }
 
   return (
-    <div>
-      <h1>🎵 Baile</h1>
-      <button onClick={createRoom}>Criar Sala</button>
+    <div className="home-container">
+      <div className="home-content">
+        <h1 className="home-title">🎵 Baile</h1>
+        <button className="create-room-btn" onClick={createRoom}>
+          Criar Sala
+        </button>
+      </div>
     </div>
   )
 }
